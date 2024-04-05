72de065ee5e67c4e31bbfc2ccad6b95aafd15482
junglimit <jaehan_jjh@naver.com>
2024-04-05 11:08:33 +0900

    # java 기초1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.println("Hello and welcome!!!!!");
+        System.out.println("해잉");
+        System.out.println(10 + 5);
 
+        int month = 5;
+        int day = 5;
+        String anni = "어린이날";
 
+        System.out.println(month+ "월 " + day+"일은 "+ anni + "입니다.");
+        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, anni);
+        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, anni);
     }
 }
