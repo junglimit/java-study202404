de2e4be454bad2c2d7ea992cb0c42bf175f4cd3d
junglimit <jaehan_jjh@naver.com>
2024-04-12 10:51:59 +0900

    # java 기초 : 패키지

--- src/day05/PackageMain.java
+++ src/day05/PackageMain.java
@@ -1,10 +1,15 @@
 package day05;
 
 // 다른 패키지에 있는 클래스를 로딩할 때 이름을 축약해서 쓰기위해 사용
+
 //import day05.juice.Apple;
 //import day05.juice.Banana;
 //import day05.juice.Peach;
+
 import day05.juice.*; // 전부 import 하기
+
+import java.util.Scanner;
+
 public class PackageMain {
     public static void main(String[] args) {
 
@@ -12,5 +17,9 @@ public class PackageMain {
         day05.juice.Apple a2 = new day05.juice.Apple(); // juice 이건 import 로 해결안됨 다쓰셈
         new Banana();
         new Peach();
+
+        // 스캐너 원래 이렇게 불러야하는데 import 로 쉽게 작성가능
+//        java.util.Scanner sc = new java.util.Scanner(System. in);
+        Scanner sc = new Scanner(System. in);
     }
 }
