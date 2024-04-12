a14720f41254320d306f0c23623ab0e3cd56a17a
junglimit <jaehan_jjh@naver.com>
2024-04-12 12:31:57 +0900

    # java 기초 : this

---
+++ src/day05/player/Main.java
@@ -0,0 +1,20 @@
+package day05.player;
+
+public class Main {
+    public static void main(String[] args) {
+        Player parking = new Player("주차왕파킹");
+        Player insultking = new Player("욕설왕뻐킹");
+
+        System.out.println("parking = " + parking);
+        System.out.println("insultking = " + insultking);
+
+        parking.attack(insultking);
+        parking.attack(insultking);
+        parking.attack(insultking);
+        parking.attack(insultking);
+
+
+        Player p1 = new Player("하하호");
+
+    }
+}
