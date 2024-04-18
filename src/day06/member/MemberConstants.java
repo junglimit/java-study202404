2c3f5f338462844e2a0a310d754a25adef487ec6
junglimit <jaehan_jjh@naver.com>
2024-04-18 16:10:59 +0900

    # java 기초 : final 연습문제

---
+++ src/day06/member/MemberConstants.java
@@ -0,0 +1,8 @@
+package day06.member;
+
+public class MemberConstants {
+
+    public static final String MALE = "M";
+    public static final String FEMALE = "F";
+
+}
--- src/day08/final_/Student.java
+++ src/day08/final_/Student.java
@@ -4,10 +4,16 @@ public class Student {
 
     String name;
     final String ssn; // 주민번호
-    String nation; // 국적
+    // 상수 선언: static + final = JS의 const
+    static final String nation = "대한민국"; // 국적
+
+//    public static final int // psfi
+//    public static final String  // psfs
 
     Student(final String ssn){
 //        ssn = "dsadd";  파라미터에도 final 을 걸어서 safety 한 코드를 만들 수 있다
         this.ssn = ssn;
+
+        double pi = Math.PI;
     }
 }
